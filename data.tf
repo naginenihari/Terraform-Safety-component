ef60392506f550c39e81bdd9a79fad0bf6936e67
naginenihari <naginenihari@gmail.com>
2025-11-11 18:03:54 +0530

    private subnet updated

--- data.tf
+++ data.tf
@@ -34,3 +34,7 @@ data "aws_ssm_parameter" "backend_alb_listener_arn" {
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name = "/${var.project_name}/${var.environment}/${var.component}/frontend_alb_listener_arn"
 }
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/${var.component}/private_subnet_ids"
+}
